e7f400e1be34c13c499a0c207ccfb3df7a676865
Paul Shipley <paul.shipley@blueyonder.co.uk>
2019-09-15 22:50:14 +0100

    Adding some comments to the code.

--- arduino-buggy.ino
+++ arduino-buggy.ino
@@ -1,12 +1,22 @@
+/*
+ * Here we set the pins that control the gate drives in the H bridge.
+ */
 #define right_pwm_pin 6
 #define right_dir_pin 7
 #define left_pwm_pin 5
 #define left_dir_pin 4
-#define led_pin 13
 
+/*
+ * This defines the rate at which the power is ramped on and off the motor. The ramp time is in milliseconds,
+ * and the step is out of a maximum of 255. In this example, we ramp 5/255% every 10ms. In other words, we
+ * go from 0% to 100% in 0.51 seconds.
+ */
 #define RAMP_STEP 5
 #define RAMP_TIME 10
 
+/*
+ * This is used to set which drive modes are available in the control code.
+ */
 enum drivemode_t {
   DRIVE_FWD,
   DRIVE_REV,
@@ -14,6 +24,10 @@ enum drivemode_t {
   TURN_RIGHT
 };
 
+/*
+ * This sets a drive instruction. It includes a drive mode, the power out of 255 to apply, and the length
+ * of time to apply this for.
+ */
 typedef struct
 {
   drivemode_t drivemode;
@@ -21,6 +35,10 @@ typedef struct
   uint16_t    duration;
 } instruction_t;
 
+/*
+ * This is the array of instructions. The main loop code will go through each of these, driving the buggy,
+ * before finally coming to a stop.
+ */
 static const instruction_t instructions[] = 
 {
   { DRIVE_FWD,  100,  5000 },
@@ -30,28 +48,37 @@ static const instruction_t instructions[] =
   { DRIVE_REV,  200,  2000 }
 };
 
+/*
+ * The setup function runs first. It simply sets the selected pins as outputs.
+ */
 void setup() {
-    pinMode(right_dir_pin, OUTPUT);
-    pinMode(right_pwm_pin, OUTPUT);
-    pinMode(left_dir_pin, OUTPUT);
-    pinMode(left_pwm_pin, OUTPUT);
-    pinMode(led_pin, OUTPUT);
+  pinMode(right_dir_pin, OUTPUT);
+  pinMode(right_pwm_pin, OUTPUT);
+  pinMode(left_dir_pin, OUTPUT);
+  pinMode(left_pwm_pin, OUTPUT);
 }
 
-
-// With the wiring I have at the moment, this makes the motors:
-// For 1 second:
-//   Left motor fwd at 50%
-//   Right motor fwd at 25%
-// For 2 seconds:
-//   Left motor rev at 50%
-//   Right motor rev at 75%
+/*
+ * This is the main loop code. It runs through each of the instructions in the array above
+ * in turn, before finally stopping the buggy.
+ */
 void loop()
 {
+  /*
+   * First we work out the size of the array. We use a cunning trick here, to take the amount of memory used by the
+   * entire instruction array, and then divide by the amount of memory used by a single instruction in the array, to
+   * give us the number of instructions.
+   */
   static const uint8_t n_instructions = sizeof(instructions) / sizeof(instructions[0]);
 
+  /*
+   * This is the start of the loop running through each instruction in the array.
+   */
   for( uint8_t idx=0; idx<n_instructions; idx++ )
   {
+    /*
+     * For each instruction, we set the drive mode, for the requried length of time.
+     */
     for( uint16_t t=0; t<instructions[idx].duration; t+=RAMP_TIME )
     {
       set_drivemode( instructions[idx].drivemode, instructions[idx].pwm );
@@ -59,6 +86,9 @@ void loop()
     }
   }
 
+  /*
+   * Once we've gone through the array, we set drive mode to stop (i.e. drive forward but with 0 power).
+   */
   while( true )
   {
     set_drivemode( DRIVE_FWD, 0 );
